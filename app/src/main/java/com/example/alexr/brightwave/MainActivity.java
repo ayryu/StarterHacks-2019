d7155cac06add78e6811f270d09f89e41b091063
Alexander Ryu <alexryu1@gmail.com>
2019-01-12 17:24:05 -0500

    Adding WeeklyGoals Activity

--- app/src/main/java/com/example/alexr/brightwave/MainActivity.java
+++ app/src/main/java/com/example/alexr/brightwave/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.alexr.brightwave;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,7 +12,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView daily = (TextView)findViewById(R.id.daily);
-        TextView weekly = (TextView)findViewById(R.id.weekly);
+        Button daily = (Button) findViewById(R.id.daily);
+        Button weekly = (Button)findViewById(R.id.weekly);
+
+        daily.setOnClickListener((v));
     }
 }
