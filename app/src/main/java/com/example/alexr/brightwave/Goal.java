ec069a96563ad8d14d57e8d00790afbbf57327ef
MrKalefruit <30781861+MrKalefruit@users.noreply.github.com>
2019-01-12 20:04:46 -0500

    Update Goal.java
    made a class^^

--- app/src/main/java/com/example/alexr/brightwave/Goal.java
+++ app/src/main/java/com/example/alexr/brightwave/Goal.java
@@ -2,9 +2,30 @@ package com.example.alexr.brightwave;
 
 public class Goal {
 
+    //var
+    private String disc = "";
+    private date end;
+    private boolean complete = false;
+
     //constructor
-    public Goal(String disc){
+    public Goal(String gdisc, date gend){
+        disc = gdisc;
+        end = gend;
+    }
 
+    //discription setter and getter values
+    public static string getdisc(){
+        return disc;
+    }
+    public static void setdisc(String newdisc){
+        disc = newdisc;
+    }
 
+    //deadline setter and getter values
+    public static string getend(){
+        return end;
+    }
+    public static void setend(date newend){
+        return disc;
     }
 }
